ea0ab80dba78fb9cef3a2497dbe1a8bf18652626
Mohamed Souare <msouaremay31@gmail.com>
2024-11-20 19:18:17 -0500

    Update ecom-join.sql

--- ecom-join.sql
+++ ecom-join.sql
@@ -1,1 +1,17 @@
+select * from rental;
+select * from customer;
+select * from inventory;
+
+select customer.first_name as fname, customer.last_name as lname
+from rental as r
+INNER join
+customer on customer.customer_id = r.customer_id;
+
+SELECT 
+    inventory.store_id, inventory.inventory_id
+FROM
+    inventory
+        INNER JOIN
+    rental ON rental.inventory_id = inventory.inventory_id;
+
 
